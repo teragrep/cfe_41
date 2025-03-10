bbfa4f65e4dcd25c3eaecddacda9eb056d565c96
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2025-03-10 15:32:38 +0200

    rebase did not go quite as planned. New commit to fix licenses and add LICENSE.

--- src/test/java/com/teragrep/cfe_41/flow/FlowResponseTest.java
+++ src/test/java/com/teragrep/cfe_41/flow/FlowResponseTest.java
@@ -1,6 +1,6 @@
 /*
  * Integration Command-line tool for Teragrep
- * Copyright (C) 2021  Suomen Kanuuna Oy
+ * Copyright (C) 2025  Suomen Kanuuna Oy
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -52,8 +52,6 @@ import jakarta.json.JsonObjectBuilder;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
